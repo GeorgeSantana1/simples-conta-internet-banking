c4e312e4ab812b4ce825847538d0b0a73c253a0a
JoaoPaulo-LD <joaopauloalencarrodrigues@gmail.com>
2020-10-15 22:36:01 -0300

    Login page created

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import LandingPage from './pages/LandingPage'
+import Login from './pages/Login'
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" component={LandingPage} />
+        <Route path="/auth" component={Login} />
       </Switch>
     </BrowserRouter>
   )
